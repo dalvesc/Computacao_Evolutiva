6ff74bf0fa2bea310f1fa91e1940dcf9e64e4752
Daniel <38465439+dalvesc@users.noreply.github.com>
2020-09-17 01:30:39 -0300

    implementando_MLPClassifier

--- PROJETO_01/ATIVIDADE_01/PROJETO_01_CE.py
+++ PROJETO_01/ATIVIDADE_01/PROJETO_01_CE.py
@@ -6,7 +6,6 @@ Created on Wed Sep  9 17:45:20 2020
 """
 
 import pandas as pd
-import random
 
 ################################### LEITURA DATASET ###################################
 #leitura do arquivo .csv
@@ -20,7 +19,7 @@ base_cancer.rename( columns={'1.1' :'Cell_Shape'}, inplace=True )
 base_cancer.rename( columns={'1.2' :'Marginal_Adhesion'}, inplace=True )    
 base_cancer.rename( columns={'2' :'Epithelial_Cell_Size'}, inplace=True )  
 base_cancer.rename( columns={'1.3' :'Bare_Nuclei'}, inplace=True )    
-base_cancer.rename( columns={'3' :'Bland_Chromatin'}, inplace=True )
+base_cancer.rename( columns={'3' :'Bland_Chromatin'},    inplace=True )
 base_cancer.rename( columns={'1.4' :'Normal_Nucleoli'}, inplace=True )
 base_cancer.rename( columns={'1.5' :'Mitoses'}, inplace=True )
 base_cancer.rename( columns={'2.1' :'Class'}, inplace=True )   
@@ -36,6 +35,8 @@ base_cancer = base_cancer.sort_index() #ordenando crescentemente
 
 
 ################################### TRATAMENTO DE DADOS ###################################
+import random
+
 #verificação de valores 'NaN'
 base_cancer.isnull().sum()
 
@@ -107,8 +108,7 @@ previsores = base_cancer.iloc[:,:].values
 ################################### CLASSIFICAÇÃO DOS DADOS ###################################
 
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier, export
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.neural_network import MLPClassifier
 
 
 #dividindo dados em teste e treinamento 
@@ -116,34 +116,22 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 previsores_treinamento, previsores_teste, previsao_treinamento, previsao_teste = train_test_split(previsores, previsao, test_size=0.25, random_state=0)
 
 #atribuindo a função a uma variável para ser utilizada
-arvore = DecisionTreeClassifier(criterion='entropy', random_state=0)
+classificador = MLPClassifier(random_state=0, max_iter=300)
 
 #treinando o modelo com os valores separados para treinamento
-arvore.fit(previsores_treinamento, previsao_treinamento)
+classificador.fit(previsores_treinamento, previsao_treinamento)
 
-#exibindo importância dos atributos, descobrindo que Cell_Size tem maior importância
-print(arvore.feature_importances_)
+#realizando previsão usando o classificador perceptron multicamadas
+classificador.predict_proba(previsores_teste)
 
-#criação de gráfico para exibição da árvore gerada utilizando o GVEdit    
-export.export_graphviz(arvore,
-                       out_file = 'arvore.dot',
-                       feature_names = ['Clump_Thickness', 'Cell_Size', 'Cell_Shape', 'Marginal_Adhesion', 'Epithelial_Cell_Size', 'Bare_Nuclei', 'Bland_Chromatin', 'Normal_Nucleoli', 'Mitoses'],
-                       class_names = ['NAO', 'SIM'],
-                       filled = True,
-                       leaves_parallel = True)   
+#precisão média nos dados de teste
+classificador.score(previsores_teste, previsao_teste)
 
-#testando o modelo com os valores separados para teste
-resultados = arvore.predict(previsores_teste)
+#número de iterações que o classificador executou
+print(classificador.n_iter_)
 
-#precisão da acurácia obtida pela árvore
-precisao = accuracy_score(previsao_teste, resultados)
-print(precisao) #0.96
+#número de camadas utilizadas
+print(classificador.n_layers_)
 
-#gerando matriz de confusão da árvore
-matriz = confusion_matrix(previsao_teste, resultados)
-print(matriz)
-
-'''
-[[113   1]
- [  6  55]]
-'''
+#função de ativação de saída
+print(classificador.out_activation_)
